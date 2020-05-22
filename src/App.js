39181c42cc170f19af6b0542b19eb1869b514541
amareshsm <amareshsm13@gmail.com>
2020-05-23 00:18:06 +0530

    Basic concepts of react hooks

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React,{useState} from 'react';
+import './App.css'
+
+
+
+
+function Todo({todo,index,completeTodo,removeTodo}){
+return (
+  <div style={{textDecoration: todo.isComplete ? 'line-through':'none'}} className="todo">
+    {todo.text}
+
+  <div>
+    <button onClick={()=> completeTodo(index)}>Complete</button>
+    <button onClick={()=> removeTodo(index)}>X</button>
+
+  </div>
+
+  </div>
+
+)
+}
+
+
+function TodoForm({addTodo}){
+ const [value, setValue] = useState('');
+
+ const handleSubmit  = e=>{
+   e.preventDefault();
+   if(!value){
+     return;
+   }else{
+     addTodo(value);
+     setValue(''); 
+   }
+ }
+   return(
+     <form onSubmit={handleSubmit}>
+       <input placeholder="Add todo" type="text" className="input" value={value} 
+       onChange={e => setValue(e.target.value)}/>
+     </form>
+   )
+
+}
+
+function App(){
+
+  const [todos, setTodos] = useState([
+    {
+    text:'Learn about React',
+    isComplete:false
+  },
+  {
+    text:'Learn about React',
+    isComplete:false
+  },
+  {
+    text:'Meet friend for lunch',
+    isComplete:false
+  },
+  {
+    text:'Build really cool todo app',
+    isComplete:false
+  }
+]);
+
+const addTodo = (text) =>{
+  const newTodos =[...todos,{ text} ];
+  setTodos(newTodos);
+}
+
+
+const completeTodo = index =>{
+  const newTodos=[...todos];
+  newTodos[index].isComplete=true;
+  setTodos(newTodos);
+}
+
+
+const removeTodo =index=>{
+  const newTodos =[...todos];
+  newTodos.splice(index,1);
+  setTodos(newTodos);
+}
+
+return(
+  <div className="app">
+    <div className="todo-list">
+       {todos.map((todo,index)=>(
+         <Todo 
+         key={index} 
+         index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          />
+       ))}
+
+       <TodoForm addTodo={addTodo} />
+    </div>
+  </div>
+)
+
+}
+
+
+export default App;
